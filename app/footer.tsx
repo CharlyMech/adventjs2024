81a8701db104ef468338918113cc4f5af2183459
CharlyMech <sanchezreciocarlos99@gmail.com>
2024-12-01 19:21:41 +0100

    feat: home wip

---
+++ app/footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+	return (
+		<footer className="w-full py-4 bg-gray-800 text-center text-white">
+			<p>&copy; 2024 Advent Calendar for Programming Challenges</p>
+		</footer>
+	);
+}
