664f660f08d516915ab7a86932bb911923666e9d
CharlyMech <sanchezreciocarlos99@gmail.com>
2024-12-01 21:20:39 +0100

    feat: framer motion to bento cards

--- app/components/card.tsx
+++ app/components/card.tsx
@@ -1,3 +1,7 @@
+'use client';
+
+import { motion } from "motion/react";
+
 type CardProps = {
 	colSpan: number;
 	rowSpan: number;
@@ -6,8 +10,9 @@ type CardProps = {
 
 export default function Card({ colSpan, rowSpan, day }: CardProps) {
 	return (
-		<div
-			className={`relative bg-transparent p-6 rounded-lg shadow-lg flex items-center justify-center overflow-hidden`}
+		<motion.div
+			whileHover={{ scale: 1.05 }} whileTap={{ scale: 0.95 }}
+			className={`relative bg-transparent p-6 rounded-lg shadow-lg flex items-center justify-center overflow-hidden cursor-pointer`}
 			style={{
 				gridColumn: `span ${colSpan} / span ${colSpan}`,
 				gridRow: `span ${rowSpan} / span ${rowSpan}`,
@@ -15,6 +20,6 @@ export default function Card({ colSpan, rowSpan, day }: CardProps) {
 		>
 			<div className="absolute inset-0 bg-black bg-opacity-10 backdrop-blur-sm rounded-lg shadow-inner"></div>
 			<span className="text-xl font-semibold z-10">Day {day}</span>
-		</div>
+		</motion.div>
 	);
 }
