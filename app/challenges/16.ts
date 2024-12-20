32a06600f9f0e1c6f07da90962b26d1ff6ac7ea9
CharlyMech <sanchezreciocarlos99@gmail.com>
2024-12-20 20:33:23 +0100

    feat: day 16 challenge

---
+++ app/challenges/16.ts
@@ -0,0 +1,42 @@
+/*
+The elves are working hard to clear paths filled with magical snow ❄️. This snow has a special property: if two identical and adjacent snow piles are found, they disappear automatically.
+
+Your task is to write a function to help the elves simulate this process. The path is represented by a string and each snow pile by a character.
+
+You need to remove all adjacent snow piles that are the same until no more moves are possible.
+
+The result should be the final path after removing all duplicate piles
+*/
+
+/**
+ * @param {string} s
+ * @returns {string}
+ */
+function removeSnow(s: string): string {
+	const stack: string[] = [];
+
+	for (const char of s) {
+		if (stack.length && stack[stack.length - 1] === char) {
+			stack.pop();
+		} else {
+			stack.push(char);
+		}
+	}
+
+	return stack.join("");
+}
+
+removeSnow("zxxzoz"); // -> "oz"
+// 1. Remove "xx", resulting in "zzoz"
+// 2. Remove "zz", resulting in "oz"
+
+removeSnow("abcdd"); // -> "abc"
+// 1. Remove "dd", resulting in "abc"
+
+removeSnow("zzz"); // -> "z"
+// 1. Remove "zz", resulting in "z"
+
+removeSnow("a"); // -> "a"
+// No duplicate piles
+
+// 5 ⭐ //
