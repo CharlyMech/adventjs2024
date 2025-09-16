6a5cd38bd917f26e0b73f9638dc6fc0f6a3b4777
CharlyMech <sanchezreciocarlos99@gmail.com>
2025-09-16 17:12:12 +0200

    feat: clean up

--- app/(home)/@modals/default.tsx
+++
@@ -1,3 +0,0 @@
-export default function HomeModalsSlot() {
-	return null;
-}
--- app/day[id]/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function DayChallengePage() {
-	return (
-		<div>
-			<h1>Day Callgenders</h1>
-			<p>Here you can find all the day callgenders.</p>
-			{/* Add your content here */}
-		</div>
-	);
-}
